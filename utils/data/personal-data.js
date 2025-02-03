0ce55a210b00d431a0dfc4f49db68cea0eb60f2f
loyalfriend1109 <danielwalker021109@gmail.com>
2025-02-03 12:25:34 -0500

    final modify

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,10 +2,10 @@ export const personalData = {
   name: "Ivan Zabusov",
   profile: '/ivanprofile.png',
   designation: "CMS, CRM Expert & Full Stack Developer",
-  description: `My name is Ivan Zabusov. 
-              I ❤️ software engineering. 
-              👩‍💻 - It's my life - it's like breathing 🌬️😌 
-              I am a skilled CMS expert and full-stack developer passionate about creating exceptional websites and web applications. 
+  description: `My name is Ivan Zabusov. <br/>
+              I ❤️ software engineering. <br/>
+              👩‍💻 - It's my life - it's like breathing 🌬️😌 <br/>
+              I am a skilled CMS expert and full-stack developer passionate about creating exceptional websites and web applications. <br/>
               My expertise spans across multiple platforms and technologies, ensuring I can deliver tailored solutions for any project.`,
 email: 'loyalfriend0615@gmail.com',
   phone: '+380 68 893 7837',
