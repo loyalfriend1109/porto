a1d68f1539b5256a6c342ce2bfdc867624dd7802
loyalfriend1109 <danielwalker021109@gmail.com>
2025-02-03 09:41:54 -0500

    Icon, photo modification

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,9 +1,9 @@
 export const personalData = {
-  name: "Roman Livinskyi",
-  profile: '/profile.png',
-  designation: "CMS, CRM Expert & Frontend Specialist",
-  description: "My name is Roman Livinskyi. I am a web developer with experience building user-friendly, stunning, and functional websites using WordPress, Shopify, Wix, Webflow, React, and Next.js. I love combining creativity with technical expertise to create digital experiences that are visually appealing, responsive, and easy to navigate. Whether it is a sleek eCommerce store or a dynamic web app, I am passionate about bringing ideas to life in a way that truly resonates with users.",
-  email: 'romanolev4@gmail.com',
-  phone: '+380 638857522',
-  address: 'street Vokzalna building 51 Zhvyrka , Lviv 80040 Ukraine',
+  name: "Ivan Zabusov",
+  profile: '/profile.jpg',
+  designation: "CMS, CRM Expert & Full Stack Developer",
+  description: "My name is Ivan Zabusov. I am a web developer with experience building user-friendly, stunning, and functional websites using WordPress, Shopify, Wix, Webflow, React, and Next.js. I love combining creativity with technical expertise to create digital experiences that are visually appealing, responsive, and easy to navigate. Whether it is a sleek eCommerce store or a dynamic web app, I am passionate about bringing ideas to life in a way that truly resonates with users.",
+  email: 'loyalfriend0615@gmail.com',
+  phone: '+380 68 893 7837',
+  address: 'Kharkiv Kharkivska Oblast Ukraine',
 }
