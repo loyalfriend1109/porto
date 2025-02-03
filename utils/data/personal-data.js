5ce931d1df09e8fc46e4663d90222f4caf0a6155
loyalfriend1109 <danielwalker021109@gmail.com>
2025-02-03 10:43:13 -0500

    hero section modify

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Ivan Zabusov",
-  profile: '/ivanprofile.png',
+  profile: './profile1.png',
   designation: "CMS, CRM Expert & Full Stack Developer",
   description: "My name is Ivan Zabusov. I am a web developer with experience building user-friendly, stunning, and functional websites using WordPress, Shopify, Wix, Webflow, React, and Next.js. I love combining creativity with technical expertise to create digital experiences that are visually appealing, responsive, and easy to navigate. Whether it is a sleek eCommerce store or a dynamic web app, I am passionate about bringing ideas to life in a way that truly resonates with users.",
   email: 'loyalfriend0615@gmail.com',
